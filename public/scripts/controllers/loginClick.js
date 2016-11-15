826068f9734ce9b37d8ff13b365fbe6d4295e0e6
Mugsy Carter <mugsycarter@gmail.com>
2016-11-15 14:46:49 -0800

    Login and signup post requests are now wired up and work correctly.

--- public/scripts/controllers/loginClick.js
+++ public/scripts/controllers/loginClick.js
@@ -1,6 +1,26 @@
-$('area').on('click', function(e) {
-  e.preventDefault();
-  $('.content').hide();
-  var regionClicked = $(this).attr('id');
-  console.log(regionClicked); 
+$('#login-form').submit(function(event) {
+  event.preventDefault();
+  var submitData = $( this ).serializeArray();
+  const submitObj = {
+    username: submitData[0].value,
+    password: submitData[1].value
+  };
+  jsonData = JSON.stringify(submitObj);
+  superagent
+    .post('/api/auth/signin')
+    .set('Content-Type', 'application/json')
+    .send(jsonData)
+    .then((res) => {
+      console.log('response is ', res);
+      $('#login-form').append('<p>Signed up as <em>'+submitObj.username+'</em></p>');
+      $('#login-form').append('<p>Your token is <em>'+res.text+'</em></p>');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
+
+
+
+
+
