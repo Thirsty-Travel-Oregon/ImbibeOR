f4be8366e22983935f1e264250d5fc18ccbe40d7
Tim Combs <tim00combs@gmail.com>
2016-11-14 20:33:50 -0800

    add click handlers for category map & buttons, import page.js into program

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -2,8 +2,8 @@
   var homeController = {};
 
   homeController.reveal = function() {
-    // $('.content).not('.static').hide();
-    $('.content).hide();
+    // $('.content').not('.static').hide();
+    $('.content').hide();
     // $('#home-page').fadeIn();
     // $('.link a').css({color:'white'});
     // $('#home-link').css({color:'grey'});
