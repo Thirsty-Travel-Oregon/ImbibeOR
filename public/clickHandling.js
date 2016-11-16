e02ce8c034cbaa34b413983aa395ef7d50d15260
Tim Combs <tim00combs@gmail.com>
2016-11-15 19:24:37 -0800

    browse by region & browse by drink type displaying on page

--- public/clickHandling.js
+++ public/clickHandling.js
@@ -23,21 +23,25 @@ $('area').on('click', function(e) {
 //all get requests
 $('.drink-type-cat li').on('click', function(e) {
   e.preventDefault();
+  $('#thread-container').empty();
   var drinkTypeClicked = $(this).attr('id');
   console.log(drinkTypeClicked);
   superagent
     .get(`/api/threads/drinkType/${drinkTypeClicked}`)
     .then((res) => {
-      console.log(res);
-      //do something with the response
-      //append them to
-      $('thread-container').append(res.body);
+      console.log(res.body);
+      const source = $('#thread-template').html();
+      const template = Handlebars.compile(source);
+      const threadObject = {thread: res.body};
+      const newHtml = template(threadObject);
+      $('#thread-container').append(newHtml);
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
+//buttons for the threads
 $('#thread-container').on('click', 'button', function(e) {
   e.preventDefault();
   var threadButtonClicked = $(this).attr('name');
@@ -53,21 +57,22 @@ $('#thread-container').on('click', 'button', function(e) {
         console.log(err);
       });
   }else if (threadButtonClicked === 'follow-user') {
+    let id = sessionStorage.getItem('storedUserID');
+    let token = 'Bearer ' + sessionStorage.getItem('storedToken');
     superagent
-      .get(`/api/${name}`)
-      .then((res) => {
-        console.log(res);
-        //do something with the response
+      .post(`/api/users/followUser/${id}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', token)
+      .send('{"userId": data-userId}')
+      .then(() => {
       })
       .catch((err) => {
         console.log(err);
       });
   }else if (threadButtonClicked === 'follow-thread') {
     superagent
-      .get(`/api/${name}`)
+      .post(`/api/users/followThread/${data-threadId}`)
       .then((res) => {
-        console.log(res);
-        //do something with the response
       })
       .catch((err) => {
         console.log(err);
@@ -78,18 +83,6 @@ $('#thread-container').on('click', 'button', function(e) {
 
   }else if (threadButtonClicked === 'create-thread') {
 
-  }
-
-  function makeAjaxCall(name) {
-      superagent
-    .get(`/api/${name}`)
-    .then((res) => {
-      console.log(res);
-      //do something with the response
-    })
-    .catch((err) => {
-      console.log(err);
-    });
   }
 
 });
