e9fd5def3bdc2eab6c1c1fa942ef27a521b71875
Michael Freeman <mjfreem1@gmail.com>
2016-11-16 16:40:28 -0800

    working on remark creation

--- public/scripts/click_handlers/signupClick.js
+++ public/scripts/click_handlers/signupClick.js
@@ -16,7 +16,7 @@ $('#signup-form').submit(function(event) {
       $('#signup-form').append('<p>Signed up as <em>'+submitObj.username+'</em></p>');
       sessionStorage.setItem('storedToken', res.body.token);
       sessionStorage.setItem('storedUserID', res.body.userId);
-      sessionStorage.setItem('storedUsername', res.body.username);
+      sessionStorage.setItem('storedUsername', res.body.userName);
       $('#signup-link').hide();
       $('#login-link').hide();
       $('#logout-link').fadeIn();
